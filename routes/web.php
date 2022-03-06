8cd482b8070cd851f1568764cab4947536d8311a
Filipe4010 <filipe.masini@outlook.com>
2022-03-06 18:26:07 -0300

    routes, controllers and views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::namespace(value:'Site')->group(function () {
+
+    Route::get('/', [HomeController::class, '__invoke']);
+    Route::get('produtos', [CategoryController::class, 'index']);
+    Route::get('produtos/{slug}', [CategoryController::class, 'show']);
+    Route::get('blog', [BlogController::class, '__invoke']);
+    Route::view('sobre', 'site.about.index');
+    Route::get('contato', [ContactController::class, 'index']);
+    Route::get('contato', [ContactController::class, 'form']);
+
+
+    //Route::get(uri:'/', action:'HomeController::class')->name('site.index');
+
+    // Route::get('produtos', 'CategoryController@index')->name('site.products');
+    // Route::get('produtos/{category}', 'CategoryController@show')->name('site.products.category');
+
+    // Route::get('blog', 'BlogController')->name('site.blog');
+
+    // Route::view('sobre', 'site.about.index')->name('site.about');
+
+    // Route::get('contato', 'ContactController@index')->name('site.contact');
+    // Route::post('contato', 'ContactController@form')->name('site.contact.form');
+
+//});
